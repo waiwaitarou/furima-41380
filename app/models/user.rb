bad2ba3ffc7b617d361465b5f50a45e15052d570
waiwaitaro <sekutonia@icloud.com>
2024-12-05 16:59:31 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
   validates :password, format: { with: VALID_PASSWORD_REGEX } 
   validates :nickname, presence: true
-  validates :familyname_kanzi, presence: true, format: {with: /\A[一-龥々ー]+\z/}
-  validates :firstname_kanzi, presence: true, format: {with: /\A[一-龥々ー]+\z/}
+  validates :familyname_kanzi, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :firstname_kanzi, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :familyname_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
   validates :firstname_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
   validates :birthday, presence: true
