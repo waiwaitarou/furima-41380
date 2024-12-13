37abb4b03ba7a3a2eb12aabf8223ac24f1c66976
waiwaitaro <sekutonia@icloud.com>
2024-12-13 22:08:43 +0900

    データベース作製

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_12_05_063208) do
-  create_table "items", charset: "utf8", force: :cascade do |t|
+  create_table "items", charset: "utf8mb3", force: :cascade do |t|
     t.string "sale_name"
     t.integer "cotegory_id"
     t.text "explanatation"
@@ -26,7 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_05_063208) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
-  create_table "users", charset: "utf8", force: :cascade do |t|
+  create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "nickname", null: false
