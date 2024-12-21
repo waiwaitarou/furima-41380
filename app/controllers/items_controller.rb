4fda57cdc058be9ac0102e5f887653e06e90c8b3
waiwaitaro <sekutonia@icloud.com>
2024-12-21 17:53:17 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,17 +18,9 @@ class ItemsController < ApplicationController
   end
   def show
     @item = Item.find(params[:id])
-    if @item.present?
-      @value = @item[:some_key]
-    else
-      @value = nil
-    end
   end
   private
   def item_params
     params.require(:item).permit(:sale_name,:explanatation,:cotegory_id,:image,:state_id,:shipping_day_id,:shipping_fee_id,:prefecture_id,:price).merge(user_id: current_user.id)
-  end
-  def my_items
-    @items = current_user.items if user_signed_in?
   end
 end
