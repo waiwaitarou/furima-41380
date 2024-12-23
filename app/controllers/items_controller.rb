c16ac150a2646d156442b5490041954318ab3577
waiwaitaro <sekutonia@icloud.com>
2024-12-23 23:05:59 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_index, only: [:edit, :update]
   def index
     @items = Item.order(created_at: :desc)
@@ -17,27 +18,29 @@ class ItemsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
   def show
-    @item = Item.find(params[:id])
   end
+
   def edit
-    @item = Item.find(params[:id])
   end
+
   def update
-    @item = Item.find(params[:id])
-      if @item.update(item_params)
-        redirect_to item_path(item_params)
-      else
-        render '/items/edit', status: :unprocessable_entity
-      end
+    if @item.update(item_params)
+      redirect_to item_path(item_params)
+    else
+      render '/items/edit', status: :unprocessable_entity
+    end
   end
+
   private
   def item_params
     params.require(:item).permit(:sale_name,:explanatation,:cotegory_id,:image,:state_id,:shipping_day_id,:shipping_fee_id,:prefecture_id,:price).merge(user_id: current_user.id)
   end
   def move_to_index
-    unless current_user == Item.find(params[:id]).user
-      redirect_to '/'
-    end
+    redirect_to '/'
+  end
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
